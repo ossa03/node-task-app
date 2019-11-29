3ea5b47bb29c52abf99dc9eb3bc0f985ca455a6f
ossa03 <kfcxd953pelo@gmail.com>
2019-11-29 20:05:38 +0900

    add remote repository

--- mongodb.js
+++ mongodb.js
@@ -1,14 +1,24 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+const { MongoClient, ObjectID } = require('mongodb');
 
-const connectionURL = `mongodb://127.0.0.1:27017`
-const databaseName = `task-manager`
+const connectionURL = `mongodb://127.0.0.1:27017`;
+const databaseName = `task-manager`;
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (err, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
     if (err) {
-        return console.log(`Unable to connect to database!`)
+        return console.log(`Unable to connect to database!`);
     }
 
-    // 特定のデータベースへの参照の取得(引数にはデータベースの名前)
-    const db = client.db(databaseName)
-})
+    const db = client.db(databaseName);
+
+    db.collection('tasks').deleteOne({ description: 'iPhone 9' })
+        .then(result => {
+            console.log(result);
+        }).catch(err => {
+            console.log(err);
+        })
+});
+
+
